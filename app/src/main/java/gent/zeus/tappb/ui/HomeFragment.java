26edaa1bcbd3b0353967a7c5808af2026d2d0d38
abeforgit <arnebertrand@gmail.com>
2019-04-14 20:38:42 +0200

    navigation??

--- app/src/main/java/gent/zeus/tappb/ui/HomeFragment.java
+++ app/src/main/java/gent/zeus/tappb/ui/HomeFragment.java
@@ -5,10 +5,14 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import gent.zeus.tappb.R;
 import gent.zeus.tappb.databinding.FragmentHomeBinding;
 
 /**
@@ -30,4 +34,9 @@ public class HomeFragment extends Fragment {
         return binding.getRoot();
     }
 
+    public void navShoppingCart(View view) {
+        Log.d("test", "test");
+        NavController navController = Navigation.findNavController(view.findViewById(R.id.nav_host_fragment));
+        navController.navigate(R.id.action_nav_home_to_placeholder);
+    }
 }
