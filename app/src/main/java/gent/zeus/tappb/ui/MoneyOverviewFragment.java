4052c2a24225fd281d23b4a596651b453aa0c87e
Robbe Van Herck <robbevanherck1@gmail.com>
2019-04-19 16:25:45 +0200

    Linked balance to Tab API

--- app/src/main/java/gent/zeus/tappb/ui/MoneyOverviewFragment.java
+++ app/src/main/java/gent/zeus/tappb/ui/MoneyOverviewFragment.java
@@ -9,24 +9,35 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.text.DecimalFormat;
+
 import androidx.navigation.fragment.NavHostFragment;
 import gent.zeus.tappb.MoneyListener;
 import gent.zeus.tappb.R;
+import gent.zeus.tappb.api.TabAPI;
 import gent.zeus.tappb.databinding.FragmentMoneyOverviewBinding;
+import gent.zeus.tappb.entity.User;
 
 
 public class MoneyOverviewFragment extends Fragment implements MoneyListener {
 
+    private DecimalFormat formatter = new DecimalFormat("#0.00");
+    private TabAPI tabAPI = new TabAPI();
+
     public MoneyOverviewFragment() {
         // Required empty public constructor
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         FragmentMoneyOverviewBinding binding = FragmentMoneyOverviewBinding.inflate(inflater, container, false);
         binding.setLifecycleOwner(this);
+        if (User.getInstance().isLoaded()) {
+            binding.balance.setText("€" + formatter.format(((double) tabAPI.getBalanceInCents()) / 100));
+        } else {
+            binding.balance.setText(getResources().getText(R.string.not_logged_in));
+        }
         binding.setHandler(this);
         return binding.getRoot();
     }
