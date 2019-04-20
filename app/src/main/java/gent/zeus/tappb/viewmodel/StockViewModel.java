c7b1a5d74fdd6164dfd9fc7568f7ce3abd49560a
abeforgit <arnebertrand@gmail.com>
2019-04-20 11:35:56 +0200

    stock api call

--- app/src/main/java/gent/zeus/tappb/viewmodel/StockViewModel.java
+++ app/src/main/java/gent/zeus/tappb/viewmodel/StockViewModel.java
@@ -1,5 +1,6 @@
 package gent.zeus.tappb.viewmodel;
 
+import gent.zeus.tappb.api.TapAPI;
 import gent.zeus.tappb.entity.Product;
 
 import java.util.ArrayList;
@@ -26,17 +27,17 @@ public class StockViewModel extends ViewModel {
     public void initializeStock() {
         stock = new MutableLiveData<>();
 
-        List<StockProduct> productList = new ArrayList<>();
-        productList.add(new StockProduct(new Product(0, "Cola", 1.20), 5));
-        productList.add(new StockProduct(new Product(1, "Ice-Tea", 1.20), 5));
-        productList.add(new StockProduct(new Product(2, "Bier", 1.20), 5));
-        productList.add(new StockProduct(new Product(3, "Mate", 1.20), 5));
-        productList.add(new StockProduct(new Product(4, "Chips", 1.20), 5));
-        productList.add(new StockProduct(new Product(5, "Bueno", 1.20), 5));
-        productList.add(new StockProduct(new Product(6, "Sapje", 1.20), 5));
-        productList.add(new StockProduct(new Product(7, "Foo", 1.20), 5));
-        productList.add(new StockProduct(new Product(8, "Bar", 1.20), 5));
-        productList.add(new StockProduct(new Product(9, "Kip", 1.20), 5));
+        List<StockProduct> productList = TapAPI.getStockProducts();
+//        productList.add(new StockProduct(new Product(0, "Cola", 1.20), 5));
+//        productList.add(new StockProduct(new Product(1, "Ice-Tea", 1.20), 5));
+//        productList.add(new StockProduct(new Product(2, "Bier", 1.20), 5));
+//        productList.add(new StockProduct(new Product(3, "Mate", 1.20), 5));
+//        productList.add(new StockProduct(new Product(4, "Chips", 1.20), 5));
+//        productList.add(new StockProduct(new Product(5, "Bueno", 1.20), 5));
+//        productList.add(new StockProduct(new Product(6, "Sapje", 1.20), 5));
+//        productList.add(new StockProduct(new Product(7, "Foo", 1.20), 5));
+//        productList.add(new StockProduct(new Product(8, "Bar", 1.20), 5));
+//        productList.add(new StockProduct(new Product(9, "Kip", 1.20), 5));
 
         ((MutableLiveData<List<StockProduct>>) stock).setValue(productList);
     }
